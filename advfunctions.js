a17814b37a16879c41d163af339b9f558b47ec56
Angelo Ibarrola <1105vino@gmail.com>
2020-03-31 14:58:29 -0700

    adv1 finished

---
+++ advfunctions.js
@@ -0,0 +1,10 @@
+
+// ADVENTURE 1 FUNCTIONS________________________________________________________________________________
+
+function showZoom(){
+    var img = document.getElementById("zoom");
+    img.style.visibility = "visible";
+}
+
+// ADVENTURE 2 FUNCTIONS_________________________________________________________________________________
+
