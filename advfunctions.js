be1bb1552fc963dbfd3097f0eb282baf73af2e04
Angelo Ibarrola <1105vino@gmail.com>
2020-03-31 19:23:32 -0700

    update deathscreen

--- advfunctions.js
+++ advfunctions.js
@@ -3,19 +3,13 @@
 
 function showZoom(){
     var img = document.getElementById("zoom");
+    document.getElementById("skipClass").disabled = true;
+
     img.style.visibility = "visible";
 }
 
 // ADVENTURE 3 FUNCTIONS_________________________________________________________________________________
 
 function costcoBackground(){
-    var text = document.getElementById("costcotext");
 
-    text.style.backgroundColor = "lightblue";
-    document.body.style.backgroundImage = "url('costco1.jpg')";
-    document.body.style.backgroundColor = "transparent";
-    document.body.style.backgroundPosition = "center";
-    document.body.style.backgroundRepeat = "no-repeat";
-    document.body.style.filter = "blur('8px')";
-    document.body.style.height = "900px";
 }
