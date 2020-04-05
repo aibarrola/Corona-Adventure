4485cf63640b90bdc324592afb41caf5714ac83a
Angelo Ibarrola <1105vino@gmail.com>
2020-04-04 19:18:39 -0700

    img src change in adv3

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ function darkmode() {
         document.body.style.backgroundColor = "black";
         
         title.style.fontFamily = "Fantasy";
-        title.innerHTML = "CORONA VIRUS EVOLUTION";
+        title.innerHTML = "COVID-20 OUTBREAK";
 
         intro.style.fontFamily = "Fantasy";
         intro.innerHTML = "COVID-19 has evolved into COVID-20. Donald Trump has put the country on an emergency lockdown. Today is the last day to grab all essential needs for your household for the next 280 days. Please be safe.";
